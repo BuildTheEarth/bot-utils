c8fc12336b228d6e9ac98b8d2836d704c2482f9a
XboxBedrock <68715625+XboxBedrock@users.noreply.github.com>
2022-07-17 15:28:35 -0700

    add hextToNum

---
+++ src/utils/hexToNum.ts
@@ -0,0 +1,11 @@
+import _ from "lodash"
+/**
+ * @description Converts a hex color to base-10
+ * @param  {string} hexCode the hex code to convert
+ * @returns {number} The hex value in base-10
+ */
+export function hexToRGB(hexCode: string): number {
+    hexCode = "0x" + _.trimStart(hexCode, "#")
+
+    return Number(hexCode)
+}
